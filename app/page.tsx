da5045f214d5321c088e57d0cc1ab437854fca70
Diego <diego.boost@gmail.com>
2025-11-04 14:39:52 +0100

    refactor per zed

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,14 @@ import { DotPattern } from "@/components/ui/dot-pattern";
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col px-3 relative space-y-3">
-      <DotPattern className="absolute inset-0 z-[-1]" width={60} height={60} cx={3} cy={3} cr={3} />
+      <DotPattern
+        className="absolute inset-0 z-[-1]"
+        width={60}
+        height={60}
+        cx={3}
+        cy={3}
+        cr={3}
+      />
       <Navbar />
       <Hero />
       <About />
